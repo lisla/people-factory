4d5eba7a5af1cdc5236d03ddd76da1607038ae50
Lucas Isla <lisla@purdue.edu>
2017-06-05 14:14:06 -0400

    Add date of birth and favorite color input; update heading and subsequent paragraphs

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const personForm = document.querySelector('form#personForm')
 
 function handleSubmit(ev){
   ev.preventDefault()
+  
   const f = ev.target
   const name = f.personName.value
-  document.querySelector('h1').textContent = name
+  const dob = f.personDOB.value
+  const favColor = f.personFavColor.value
+
+  document.querySelector('h1').textContent = name + ', Date of Birth: ' + dob
+  document.querySelector('p#blank').textContent = 'Hi, my name is ' + name + ', and I was born on ' + dob
+  
+  const favColorParagraph = document.querySelector('p#favColor')
+  favColorParagraph.style.color = favColor;
+  favColorParagraph.style.fontStyle = 'italic'
 }
 
 personForm.addEventListener('submit', handleSubmit)
